2f970772df76a0ec9e07cc175c39b1fd84a8b865
jhjeon-0to1 <jhjeon@0to1partners.com>
2021-07-13 16:34:41 +0900

    docker`:

--- dokcer-deploy.sh
+++
@@ -1,8 +0,0 @@
-docker login -u janus1221 -p wlgns7124!
-
-docker build -t janus1221/next-test-3 .
-
-docker tag janus1221/next-test-3 janus1221/next-test-3:latest
-
-docker push janus1221/next-test-3
-docker push janus1221/next-test-3:latest
